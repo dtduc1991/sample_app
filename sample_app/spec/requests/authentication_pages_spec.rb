dc954c58ebb92f2afb28531e121ee10fbb58dc67
Duc Dang Trong <dtduc1991@gmail.com>
2012-06-27 12:17:43 +0700

    Add user following

--- sample_app/spec/requests/authentication_pages_spec.rb
+++ sample_app/spec/requests/authentication_pages_spec.rb
@@ -68,17 +68,17 @@ describe "AuthenticationPages" do
 			end
 			describe "in the Microposts controller" do
 
-			        describe "submitting to the create action" do
-			          before { post microposts_path }
-			          specify { response.should redirect_to(signin_path) }
-			        end
+				describe "submitting to the create action" do
+					before { post microposts_path }
+					specify { response.should redirect_to(signin_path) }
+				end
 
-			        describe "submitting to the destroy action" do
-			          before { delete micropost_path(FactoryGirl.create(:micropost)) }
-			          specify { response.should redirect_to(signin_path) }
-			        end
-			      end
-			      
+				describe "submitting to the destroy action" do
+					before { delete micropost_path(FactoryGirl.create(:micropost)) }
+					specify { response.should redirect_to(signin_path) }
+				end
+			end
+
 			describe "in the Users controller" do
 
 				describe "visiting the edit page" do
@@ -96,6 +96,27 @@ describe "AuthenticationPages" do
 					it { should have_selector('title', text: 'Sign in') }
 				end
 				
+				describe "visiting the following page" do
+					before { visit following_user_path(user) }
+					it { should have_selector('title', text: 'Sign in') }
+				end
+
+				describe "visiting the followers page" do
+					before { visit followers_user_path(user) }
+					it { should have_selector('title', text: 'Sign in') }
+				end
+			end
+
+			describe "in the Relationships controller" do
+				describe "submitting to the create action" do
+					before { post relationships_path }
+					specify { response.should redirect_to(signin_path) }
+				end
+
+				describe "submitting to the destroy action" do
+					before { delete relationship_path(1) }
+					specify { response.should redirect_to(signin_path) }          
+				end
 			end
 		end
 
